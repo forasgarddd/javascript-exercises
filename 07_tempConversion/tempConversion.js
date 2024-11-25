abf67895100a4ba2d99d9ea91482c9828218a3e0
forasgarddd <forbattlenetonly14@gmail.com>
2024-11-25 00:14:32 +0000

    Complete exercises 1-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let converted = ((temperature - 32) * 5)/9;
+  return parseFloat(converted.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let converted = (temperature * 9 / 5) + 32;
+  return parseFloat(converted.toFixed(1));
 };
 
 // Do not edit below this line
